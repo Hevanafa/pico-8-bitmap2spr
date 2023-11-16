37bb78f1a853040809c920d5527eed5247e2be94
Hevanafa <dhevansafa4@hotmail.com>
2023-11-16 13:58:56 +0700

    added preview & SPR processing

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,51 +23,59 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.pbSource = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pbPreview = New System.Windows.Forms.PictureBox()
+        Me.txbOutput = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rb8x8 = New System.Windows.Forms.RadioButton()
+        Me.rb16x16 = New System.Windows.Forms.RadioButton()
+        Me.rb32x32 = New System.Windows.Forms.RadioButton()
+        Me.rb64x64 = New System.Windows.Forms.RadioButton()
         CType(Me.pbSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbPreview, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'pbSource
         '
         Me.pbSource.Location = New System.Drawing.Point(13, 29)
-        Me.pbSource.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.pbSource.Margin = New System.Windows.Forms.Padding(4)
         Me.pbSource.Name = "pbSource"
         Me.pbSource.Size = New System.Drawing.Size(128, 128)
         Me.pbSource.TabIndex = 0
         Me.pbSource.TabStop = False
         '
-        'PictureBox2
+        'pbPreview
         '
-        Me.PictureBox2.Location = New System.Drawing.Point(149, 29)
-        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(128, 128)
-        Me.PictureBox2.TabIndex = 1
-        Me.PictureBox2.TabStop = False
+        Me.pbPreview.Location = New System.Drawing.Point(149, 29)
+        Me.pbPreview.Margin = New System.Windows.Forms.Padding(4)
+        Me.pbPreview.Name = "pbPreview"
+        Me.pbPreview.Size = New System.Drawing.Size(128, 128)
+        Me.pbPreview.TabIndex = 1
+        Me.pbPreview.TabStop = False
         '
-        'TextBox1
+        'txbOutput
         '
-        Me.TextBox1.Location = New System.Drawing.Point(13, 216)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(264, 156)
-        Me.TextBox1.TabIndex = 2
+        Me.txbOutput.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txbOutput.Location = New System.Drawing.Point(13, 216)
+        Me.txbOutput.Margin = New System.Windows.Forms.Padding(4)
+        Me.txbOutput.Multiline = True
+        Me.txbOutput.Name = "txbOutput"
+        Me.txbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txbOutput.Size = New System.Drawing.Size(264, 156)
+        Me.txbOutput.TabIndex = 2
         '
-        'Button1
+        'btnGenerate
         '
-        Me.Button1.Location = New System.Drawing.Point(13, 165)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(264, 43)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Generate SPR code"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnGenerate.Location = New System.Drawing.Point(13, 165)
+        Me.btnGenerate.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(264, 43)
+        Me.btnGenerate.TabIndex = 3
+        Me.btnGenerate.Text = "Generate SPR code"
+        Me.btnGenerate.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -87,33 +95,96 @@ Partial Class Form1
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Preview"
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rb64x64)
+        Me.GroupBox1.Controls.Add(Me.rb32x32)
+        Me.GroupBox1.Controls.Add(Me.rb16x16)
+        Me.GroupBox1.Controls.Add(Me.rb8x8)
+        Me.GroupBox1.Location = New System.Drawing.Point(284, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(116, 131)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sprite Size"
+        '
+        'rb8x8
+        '
+        Me.rb8x8.AutoSize = True
+        Me.rb8x8.Checked = True
+        Me.rb8x8.Location = New System.Drawing.Point(6, 22)
+        Me.rb8x8.Name = "rb8x8"
+        Me.rb8x8.Size = New System.Drawing.Size(48, 20)
+        Me.rb8x8.TabIndex = 0
+        Me.rb8x8.TabStop = True
+        Me.rb8x8.Text = "8×8"
+        Me.rb8x8.UseVisualStyleBackColor = True
+        '
+        'rb16x16
+        '
+        Me.rb16x16.AutoSize = True
+        Me.rb16x16.Location = New System.Drawing.Point(6, 48)
+        Me.rb16x16.Name = "rb16x16"
+        Me.rb16x16.Size = New System.Drawing.Size(62, 20)
+        Me.rb16x16.TabIndex = 1
+        Me.rb16x16.Text = "16×16"
+        Me.rb16x16.UseVisualStyleBackColor = True
+        '
+        'rb32x32
+        '
+        Me.rb32x32.AutoSize = True
+        Me.rb32x32.Location = New System.Drawing.Point(6, 74)
+        Me.rb32x32.Name = "rb32x32"
+        Me.rb32x32.Size = New System.Drawing.Size(62, 20)
+        Me.rb32x32.TabIndex = 2
+        Me.rb32x32.Text = "32×32"
+        Me.rb32x32.UseVisualStyleBackColor = True
+        '
+        'rb64x64
+        '
+        Me.rb64x64.AutoSize = True
+        Me.rb64x64.Location = New System.Drawing.Point(6, 100)
+        Me.rb64x64.Name = "rb64x64"
+        Me.rb64x64.Size = New System.Drawing.Size(62, 20)
+        Me.rb64x64.TabIndex = 3
+        Me.rb64x64.Text = "64×64"
+        Me.rb64x64.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(290, 385)
+        Me.ClientSize = New System.Drawing.Size(412, 385)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.txbOutput)
+        Me.Controls.Add(Me.pbPreview)
         Me.Controls.Add(Me.pbSource)
         Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.pbSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbPreview, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents pbSource As PictureBox
-    Friend WithEvents PictureBox2 As PictureBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents pbPreview As PictureBox
+    Friend WithEvents txbOutput As TextBox
+    Friend WithEvents btnGenerate As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rb64x64 As RadioButton
+    Friend WithEvents rb32x32 As RadioButton
+    Friend WithEvents rb16x16 As RadioButton
+    Friend WithEvents rb8x8 As RadioButton
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        colours = colours_raw.Select(Function(c) Color.FromArgb(c)).ToArray
+        colours = colours_raw.Select(Function(c) Color.FromArgb(255, Color.FromArgb(c))).ToArray
     End Sub
 
     Dim colours_raw%() = New Integer() {
@@ -32,12 +32,40 @@
         End If
     End Sub
 
-    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+
+    Function GetNearest(colour As Color) As Color
+        Dim diff% = 1000
+        Dim last_c As Color
+
+        Try
+            For Each c In colours
+                last_c = c
+                Dim temp_diff% = {Int(c.R) - colour.R, Int(c.G) - colour.G, Int(c.B) - colour.B}.Sum(Function(x) Math.Abs(x))
+
+                If diff > temp_diff Then
+                    diff = temp_diff
+                    GetNearest = c
+                End If
+            Next
+        Catch oex As OverflowException
+            Debug.Print(colour.ToString)
+            Debug.Print(last_c.ToString)
+            GetNearest = Color.Black
+        Catch ex As Exception
+            Debug.Print(colour.ToString)
+            GetNearest = Color.Black
+        End Try
+    End Function
+
+
+    Dim edited As Bitmap
+
+    Private Async Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
         Dim files$() = e.Data.GetData(DataFormats.FileDrop)
         Dim first = files(0)
 
         Try
-            Dim img = Image.FromFile(first)
+            Dim img As Bitmap = Image.FromFile(first)
 
             If img.Width > 64 OrElse img.Height > 64 Then
                 Throw New FormatException("The size can't be more than 64x64.")
@@ -45,9 +73,22 @@
 
             pbSource.Image = img
 
-            ' Todo: redraw
+            ' Redraw
+            If edited IsNot Nothing Then _
+                edited.Dispose()
 
+            edited = New Bitmap(img)
+            Dim colour As Color
 
+            For y = 0 To img.Height - 1
+                For x = 0 To img.Width - 1
+                    colour = img.GetPixel(x, y)
+                    edited.SetPixel(x, y, GetNearest(colour))
+            Next x, y
+
+            'edited.Save("temp.png", Imaging.ImageFormat.Png)
+
+            RedrawPreview()
 
         Catch ex As Exception
             MessageBox.Show("An exception occurred: " + ex.Message)
@@ -55,7 +96,68 @@
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
+    Sub RedrawPreview()
+        If edited Is Nothing Then Return
+
+        Dim size% = GetConversionSize()
+        Dim cropped As New Bitmap(size, size)
+        Using g = Graphics.FromImage(cropped)
+            g.DrawImage(edited, 0, 0)
+        End Using
+        pbPreview.Image = cropped
+    End Sub
+
+
+    Function GetConversionSize%()
+        GetConversionSize = IIf(rb8x8.Checked, 8,
+                        IIf(rb16x16.Checked, 16,
+                            IIf(rb32x32.Checked, 32,
+                                IIf(rb64x64.Checked, 64, 1))))
+    End Function
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim img As Bitmap = pbSource.Image
+
+        Dim size% = GetConversionSize()
+
+        Dim padding% = IIf(size = 8, 2,
+                           IIf(size = 16, 8,
+                               IIf(size = 32, 32,
+                                   IIf(size = 64, 128, 1))))
+
+        ' last padding:
+        ' 8x8: 2
+        ' 16x16: 8
+        ' 32x32: 32
+        ' 64x64: 128
+        txbOutput.Text = String.Join("", Enumerable.Range(0, size).Select(
+            Function(y)
+                Return String.Join("", Enumerable.Range(0, size).Select(
+                Function(x)
+                    Try
+                        Return Array.IndexOf(colours, GetNearest(img.GetPixel(x, y))).ToString("x")
+                    Catch ex As Exception
+                        Return 0
+                    End Try
+                End Function))
+            End Function)) + StrDup(padding, "0"c)
+
+    End Sub
+
+    Private Sub rb8x8_CheckedChanged(sender As Object, e As EventArgs) Handles rb8x8.CheckedChanged
+        RedrawPreview()
+    End Sub
+
+    Private Sub rb16x16_CheckedChanged(sender As Object, e As EventArgs) Handles rb16x16.CheckedChanged
+        RedrawPreview()
+    End Sub
+
+    Private Sub rb32x32_CheckedChanged(sender As Object, e As EventArgs) Handles rb32x32.CheckedChanged
+        RedrawPreview()
+    End Sub
+
+    Private Sub rb64x64_CheckedChanged(sender As Object, e As EventArgs) Handles rb64x64.CheckedChanged
+        RedrawPreview()
     End Sub
 End Class
